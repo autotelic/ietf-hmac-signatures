10c9bfc908a48d45d2c82251d9e01be31af0b8c1
jeff-sexton <sexton.jeffp@gmail.com>
2020-10-14 14:03:56 -0700

    Create basic client request example

--- examples/basic.js
+++ examples/basic.js
@@ -2,41 +2,77 @@
 
 const fetch = require('cross-fetch')
 
+const { signRequest } = require('../src')
+
 // const {
 //   constructSignatureString,
 //   extractSignature
 // } = require('../src')
+const DEFAULT_ENCODING = 'base64'
 
-// const pluginOptions = {
-//   constructSignatureString,
-//   extractSignature,
-//   getAlgorithm: () => 'sha512',
-//   getSignatureEncoding: () => 'base64',
-//   sharedSecret: 'topSecret',
-//   algorithmMap: {
-//     hs2019: {
-//       'test-key-a': 'sha512',
-//       'test-key-b': 'sha256'
-//     }
+const pluginOptions = {
+  // constructSignatureString,
+  // extractSignature,
+  getAlgorithm: () => 'sha512',
+  getSignatureEncoding: () => DEFAULT_ENCODING,
+  sharedSecret: 'topSecret',
+  algorithmMap: {
+    hs2019: {
+      'test-key-a': 'sha512',
+      'test-key-b': 'sha256'
+    }
+  },
+  digestAlgorithm: 'sha-512',
+  digestEncoding: DEFAULT_ENCODING
+}
+
+// ;(async () => {
+//   try {
+//     const req = {}
+//     req.method = 'POST'
+//     req.body = JSON.stringify({ hello: 'world' })
+//     req.headers = {}
+//     req.headers['Content-Type'] = 'application/json'
+//     req.headers.Digest = 'sha-512=+PtokCNHosgo04ww4cNhd4yJxhMjLzWjDAKtKwQZDT4Ef9v/PrS/+BQLX4IX5dZkUMK/tQo7Uyc68RkhNyCZVg=='
+
+//     req.headers.Signature = 'keyId="test-key-a", algorithm="hs2019", headers="(request-target) (created) (expires) host digest content-type", signature="pQul5YFrqv76Zq2bE1kWjJfFGnTu0MwU7X7c8MWDswAI5V7dROqKbBWKUGcoysxujgTqkJo/Eg74x34o54hqRg==", created=1402170695, expires=1402170895'
+
+//     console.log('\n\n ********* \n\n Request is:\n', req)
+
+//     const res = await fetch('http://localhost:3000', req)
+
+//     console.log('\n\n******************\n\nWorking Response:\n', res)
+//     const message = await res.json()
+//     console.log('\n\nResponse Message: ', message)
+//     console.log('\n\n******************\n\n')
+//   } catch (error) {
+//     console.error(error)
 //   }
-// }
-
+// })()
 ;(async () => {
   try {
-    const req = {}
-    req.method = 'POST'
-    req.body = JSON.stringify({ hello: 'world' })
-    req.headers = {}
-    req.headers['Content-Type'] = 'application/json'
-    req.headers.Digest = 'sha-512=+PtokCNHosgo04ww4cNhd4yJxhMjLzWjDAKtKwQZDT4Ef9v/PrS/+BQLX4IX5dZkUMK/tQo7Uyc68RkhNyCZVg=='
+    const req = new fetch.Request('http://localhost:3000/?id=1', {
+      method: 'POST',
+      body: JSON.stringify({ hello: 'world' })
+    })
+    // console.log(req)
+    // req.body = JSON.stringify({ hello: 'world' })
+    // req.headers = {}
+    req.headers.set('Content-Type', 'application/json')
+    // req.headers.set('Digest', 'sha-512=+PtokCNHosgo04ww4cNhd4yJxhMjLzWjDAKtKwQZDT4Ef9v/PrS/+BQLX4IX5dZkUMK/tQo7Uyc68RkhNyCZVg==')
 
-    req.headers.Signature = 'keyId="test-key-a", algorithm="hs2019", headers="(request-target) (created) (expires) host digest content-type", signature="pQul5YFrqv76Zq2bE1kWjJfFGnTu0MwU7X7c8MWDswAI5V7dROqKbBWKUGcoysxujgTqkJo/Eg74x34o54hqRg==", created=1402170695, expires=1402170895'
+    pluginOptions.signatureMaterial = {
+      headers: ['(request-target)', '(created)', '(expires)', 'host', 'digest', 'content-type'],
+      expires: 1402170895
+    }
 
-    console.log('\n\n ********* \n\n Request is:\n', req)
+    const signedReq = signRequest(req, pluginOptions)
+    // console.log('\n\n ********* \n\n Request is:\n', signedReq)
+    // console.log('\n\n ********* \n\n Request is:\n', signedReq.headers)
 
-    const res = await fetch('http://localhost:3000', req)
+    const res = await fetch(signedReq)
 
-    console.log('\n\n******************\n\nWorking Response:\n', res)
+    // console.log('\n\n******************\n\nWorking Response:\n', res)
     const message = await res.json()
     console.log('\n\nResponse Message: ', message)
     console.log('\n\n******************\n\n')
