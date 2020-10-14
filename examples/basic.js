6661fc9c13f924ee7313e920f21d9623de22b332
jeff-sexton <sexton.jeffp@gmail.com>
2020-10-14 16:51:07 -0700

    Add optional property comments

--- examples/basic.js
+++ examples/basic.js
@@ -25,7 +25,12 @@ const { signRequest } = require('../src')
       signedHeaders: ['(request-target)', '(created)', '(expires)', 'host', 'digest', 'content-type'],
       keyId: 'test-key-b',
       algorithmName: 'hs2019'
-      // expiryOffset: 300000 // Optional - Default is 300000 if unspecified
+      // expiryOffset: 300000, // Optional - Default is 300000 if unspecified
+      // digestEncoding: 'base64', // Optional - Default is base64 if unspecified
+      // digestAlgorithm: 'sha-512' // Optional - Default is sha-512 if unspecified
+      // getAlgorithm: (req, options) => 'sha512', // Optional - Default getAlgorithm used if unspecified
+      // constructSignatureString: (req, options) => {}, // Optional - Default constructSignatureString used if unspecified
+      // constructDigestString: (req, options) => {}, // Optional - Default constructDigestString used if unspecified
     }
 
     const signedReq = signRequest(req, options)
