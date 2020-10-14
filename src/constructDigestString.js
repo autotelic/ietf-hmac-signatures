29d0bbec62929c8582800261df087fb2a07da4dd
jeff-sexton <sexton.jeffp@gmail.com>
2020-10-14 14:06:13 -0700

    Create constructDigestString method

---
+++ src/constructDigestString.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const crypto = require('crypto')
+
+const constructDigestString = (req, options) => {
+  const { digestEncoding, digestAlgorithm } = options
+  const formattedAlgorithm = digestAlgorithm.toLowerCase().split('-').join('')
+  const { body } = req
+
+  const calculatedDigest = calculateDigest({
+    algorithm: formattedAlgorithm,
+    message: body,
+    encoding: digestEncoding
+  })
+
+  return `${digestAlgorithm}=${calculatedDigest}`
+}
+
+const calculateDigest = ({ algorithm, message, encoding }) =>
+  crypto.createHash(algorithm)
+    .update(message)
+    .digest(encoding)
+    .toString()
+
+module.exports = constructDigestString
