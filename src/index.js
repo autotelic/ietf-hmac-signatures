931501f6001f6bcaf0ea3ba6017c20164d91b1cd
jeff-sexton <sexton.jeffp@gmail.com>
2020-11-02 13:21:29 -0800

    fix: remove extra export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 'use strict'
 
 const createSignRequest = require('./createSignRequest')
-const getAlgorithm = require('./getAlgorithm')
 
 module.exports = {
-  createSignRequest,
-  getAlgorithm
+  createSignRequest
 }
