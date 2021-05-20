9c73d00fb587d9332b9d91a3365b03f95ef1310c
jeff-sexton <sexton.jeffp@gmail.com>
2021-05-20 11:43:07 -0700

    feat: initial typescript setup

---
+++ types/ieft-hmac-signatures.d.ts
@@ -0,0 +1,23 @@
+// /**
+//  * Options for the OpenTelemetry plugin.
+//  */
+// export interface OpenTelemetryPluginOptions {
+//   readonly serviceName?: string,
+//   readonly exposeApi?: boolean,
+//   readonly formatSpanName?: (serviceName: string, raw: FastifyRequest['raw']) => string,
+//   readonly formatSpanAttributes?: {
+//     readonly request?: (request: FastifyRequest) => SpanAttributes,
+//     readonly reply?: (reply: FastifyReply) => SpanAttributes,
+//     readonly error?: (error: Error) => SpanAttributes,
+//   },
+//   readonly wrapRoutes?: boolean | string[],
+//   readonly ignoreRoutes?: string[],
+// }
+
+// declare const fastifyOpenTelemetry: FastifyPluginCallback<OpenTelemetryPluginOptions>
+
+// export default fastifyOpenTelemetry
+
+declare module '@autotelic/ietf-hmac-signatures' {
+    const noTypesYet: any;
+    export default noTypesYet;}
